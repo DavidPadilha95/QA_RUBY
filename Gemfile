67fbf0de537dd9fbc78ea6f50f27e3db8f83a1b6
DavidPadilha95 <david.padilha@hotmail.com>
2021-05-03 17:49:30 -0300

    Novas implementações

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 
 gem 'cucumber', '5.2'
+gem 'capybara', '3.3'
+gem 'selenium-webdriver', '3.142'
--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,5 +1,7 @@
 Dado('que acesso a página de cadastro') do
-    pending # Write code here that turns the phrase above into concrete actions
+    visit "https://hportal.webmotors.com.br/garagem/autopago/pesquisa?typesearch=vendedor&step=1&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzODgyIiwianRpIjoiMzM5MTU2YzctODc3Mi00ODFhLThjOWEtMTRiODc3ZmYzNzhmIiwiY2xpZW50IjoiMSIsImVtYWlsIjoibG8wRFROOEdlY0dHTnEyMG9aaHFsSGxsbHVUY0M4bzZKaUg4cncvSHVvUXZjUVE1TGl4YjR3PT0iLCJuYmYiOjE2MTY0MTkyMTEsImV4cCI6MTYxODIzMzYxMSwiaXNzIjoid2VibW90b3JzLmNvbS5iciIsImF1ZCI6IndlYm1vdG9ycy5jb20uYnIifQ.ea-_-g3_g82FAzmMiUSvtrcDcg2mCFPBk2KFW9vH3Zo"
+    find("#initialSign_1").click
+    sleep 10
 end
   
 Quando('submeto o meu cadastro completo') do
